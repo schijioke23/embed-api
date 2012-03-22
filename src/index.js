b7429b14f0a014bcdc74b73919f1fab38c3015ab
giannif <giannif@gmail.com>
2012-03-22 13:13:40 -0400

    updating docs, moving createPlayers, allowing createPlayers to take a config and an events object

--- src/index.js
+++ src/index.js
@@ -6,12 +6,37 @@ var MTVNPlayer = MTVNPlayer || {};
 if (!MTVNPlayer.Player) {
     /**
      * Events dispatched by {@link MTVNPlayer.Player}.
-     * attach them like so:
-     * 
-     * `player.bind("onMetadata",function(event){// your code here});`
      *
-     *  All events have a target property (event.target) which is the player that dispatched the event.
-     *  Some events have a data property (event.data) which contains data specific to the event.
+     * All events have a target property (event.target) which is the player that dispatched the event.
+     * Some events have a data property (event.data) which contains data specific to the event.
+     * 
+     * # How to listen to events
+     * Attached to player instance via {@link MTVNPlayer.Player#bind}:
+     *      player.bind("onMetadata",function(event) {  
+     *             var metadata = event.data;
+     *          }    
+     *      });
+     * Passed in as an Object to the constructor {@link MTVNPlayer.Player}:
+     *      var player = new MTVNPlayer.Player("video-player",config,{
+     *              onMetadata:function(event) {
+     *                  var metadata = event.data;
+     *              }
+     *      });
+     * Passed as an Object into {@link MTVNPlayer#createPlayers}
+     *      MTVNPlayer.createPlayers("div.MTVNPlayer",config,{
+     *              onMetadata:function(event) {
+     *                  var metadata = event.data;
+     *                  // player that dispatched the event
+     *                  var player = event.player;
+     *                  var uri = event.player.config.uri;
+     *              }
+     *      });
+     * Attached to player from {@link MTVNPlayer#onPlayer}
+     *      MTVNPlayer.onPlayer(function(player){
+     *              player.bind("onMetadata",function(event) {  
+     *                  var metadata = event.data;
+     *              }    
+     *      });
      * 
      */
     MTVNPlayer.Events = {
@@ -85,6 +110,7 @@ if (!MTVNPlayer.Player) {
             swfobjectBase = baseURL + "player/api/swfobject/",
             html5 = null,
             flash = null,
+            selector = null,
             jQuery = window.jQuery,
             throwError = function(message) {
                 throw new Error("Embed API:" + message);
@@ -652,14 +678,16 @@ if (!MTVNPlayer.Player) {
                 return embedCode;
             },
             copyProperties = function(toObj, fromObj) {
-                for (var prop in fromObj) {
-                    if (fromObj.hasOwnProperty(prop)) {
-                        if (fromObj[prop]) {
-                            var propName = prop.toLowerCase();
-                            if (propName === "flashvars" || propName === "attributes" || propName === "params") {
-                                copyProperties(toObj[prop], fromObj[prop]);
-                            } else {
-                                toObj[prop] = fromObj[prop];
+                if (fromObj) {
+                    for (var prop in fromObj) {
+                        if (fromObj.hasOwnProperty(prop)) {
+                            if (fromObj[prop]) {
+                                var propName = prop.toLowerCase();
+                                if (propName === "flashvars" || propName === "attributes" || propName === "params") {
+                                    copyProperties(toObj[prop], fromObj[prop]);
+                                } else {
+                                    toObj[prop] = fromObj[prop];
+                                }
                             }
                         }
                     }
@@ -714,11 +742,17 @@ if (!MTVNPlayer.Player) {
          * @member MTVNPlayer
          * If you want to know when players are created that you do not create, pass in a callback.
          * @param {Function} callback A callback fired when every player is created.
+         * 
+         *     MTVNPlayer.onPlayer(function(player){
+         *         player.bind("onReady",function(event) {  
+         *             // do something
+         *         }    
+         *     });
          */
         MTVNPlayer.onPlayer = function(callback) {
             onPlayerCallbacks.push(callback);
         };
-         /**
+        /**
          * @member MTVNPlayer
          * (Available in 1.6.0) Remove a callback registered width {MTVNPlayer.onPlayer}
          * @param {Function} callback A callback fired when every player is created.
@@ -742,6 +776,157 @@ if (!MTVNPlayer.Player) {
             }
             return result;
         };
+        /**
+         * @member MTVNPlayer
+         * Create players from elements in the page.
+         * @param {String} selector default is "div.MTVNPlayer"
+         * @param {Object} config {@link MTVNPlayer.Player#config}
+         * @param {Object} events {@link MTVNPlayer.Events}
+         *
+         * Example:
+         *      <div class="MTVNPlayer" data-contenturi="mgid:cms:video:nick.com:920786"/>
+         *      <script type="text/javascript">
+         *              MTVNPlayer.createPlayers("div.MTVNPlayer",{width:640,height:320})
+         *      </script>
+         *  With events:
+         *      <div class="MTVNPlayer" data-contenturi="mgid:cms:video:nick.com:920786"/>
+         *      <script type="text/javascript">
+         *              MTVNPlayer.createPlayers("div.MTVNPlayer",{width:640,height:320},{
+         *                  onPlayheadUpdate:function(event) {
+         *                      // do something
+         *                  }
+         *              });
+         *      </script>
+         */
+        MTVNPlayer.createPlayers = function(selectorQuery, config, events) {
+            if (!selectorQuery) {
+                selectorQuery = "div.MTVNPlayer";
+            }
+            if (!selector) {
+                /**
+                 * micro-selector 
+                 * @method selector
+                 * @private
+                 * author:  Fabio Miranda Costa
+                 * github:  fabiomcosta
+                 * twitter: @fabiomiranda
+                 * license: MIT-style license
+                 */
+                (function(document) {
+                    var elements, parsed, parsedClasses, parsedPseudos, pseudos = {},
+                        context, currentDocument, reTrim = /^\s+|\s+$/g;
+                    var supports_querySelectorAll = !! document.querySelectorAll;
+                    var $u = function(selector, _context, append) {
+                            elements = append || [];
+                            context = _context || $u.context;
+                            if (supports_querySelectorAll) {
+                                try {
+                                    arrayFrom(context.querySelectorAll(selector));
+                                    return elements;
+                                } catch (e) {}
+                            }
+                            currentDocument = context.ownerDocument || context;
+                            parse(selector.replace(reTrim, ''));
+                            find();
+                            return elements;
+                        };
+                    var matchSelector = function(node) {
+                            if (parsed.tag) {
+                                var nodeName = node.nodeName.toUpperCase();
+                                if (parsed.tag == '*') {
+                                    if (nodeName < '@') return false; // Fix for comment nodes and closed nodes
+                                } else {
+                                    if (nodeName != parsed.tag) return false;
+                                }
+                            }
+                            if (parsed.id && node.getAttribute('id') != parsed.id) {
+                                return false;
+                            }
+                            if ((parsedClasses = parsed.classes)) {
+                                var className = (' ' + node.className + ' ');
+                                for (var i = parsedClasses.length; i--;) {
+                                    if (className.indexOf(' ' + parsedClasses[i] + ' ') < 0) return false;
+                                }
+                            }
+                            if ((parsedPseudos = parsed.pseudos)) {
+                                for (var i = parsedPseudos.length; i--;) {
+                                    var pseudoClass = pseudos[parsedPseudos[i]];
+                                    if (!(pseudoClass && pseudoClass.call($u, node))) return false;
+                                }
+                            }
+                            return true;
+                        };
+                    var find = function() {
+                            var parsedId = parsed.id,
+                                merge = ((parsedId && parsed.tag || parsed.classes || parsed.pseudos) || (!parsedId && (parsed.classes || parsed.pseudos))) ? arrayFilterAndMerge : arrayMerge;
+                            if (parsedId) {
+                                var el = currentDocument.getElementById(parsedId);
+                                if (el && (currentDocument === context || contains(el))) {
+                                    merge([el]);
+                                }
+                            } else {
+                                merge(context.getElementsByTagName(parsed.tag || '*'));
+                            }
+                        };
+                    var parse = function(selector) {
+                            parsed = {};
+                            while ((selector = selector.replace(/([#.:])?([^#.:]*)/, parser))) {};
+                        };
+                    var parser = function(all, simbol, name) {
+                            if (!simbol) {
+                                parsed.tag = name.toUpperCase();
+                            } else if (simbol == '#') {
+                                parsed.id = name;
+                            } else if (simbol == '.') {
+                                if (parsed.classes) {
+                                    parsed.classes.push(name);
+                                } else {
+                                    parsed.classes = [name];
+                                }
+                            } else if (simbol == ':') {
+                                if (parsed.pseudos) {
+                                    parsed.pseudos.push(name);
+                                } else {
+                                    parsed.pseudos = [name];
+                                }
+                            }
+                            return '';
+                        };
+                    var slice = Array.prototype.slice;
+                    var arrayFrom = function(collection) {
+                            elements = slice.call(collection, 0);
+                        };
+                    var arrayMerge = function(collection) {
+                            for (var i = 0, node; node = collection[i++];) {
+                                elements.push(node);
+                            }
+                        };
+                    try {
+                        slice.call(document.documentElement.childNodes, 0);
+                    } catch (e) {
+                        arrayFrom = arrayMerge;
+                    }
+                    var arrayFilterAndMerge = function(found) {
+                            for (var i = 0, node; node = found[i++];) {
+                                if (matchSelector(node)) elements.push(node);
+                            }
+                        };
+                    var contains = function(node) {
+                            do {
+                                if (node === context) return true;
+                            } while ((node = node.parentNode));
+                            return false;
+                        };
+                    $u.pseudos = pseudos;
+                    $u.context = document;
+                    selector = $u;
+                })(document);
+            }
+            var elements = selector(selectorQuery);
+            for (var i = 0, len = elements.length; i < len; i++) {
+                new MTVNPlayer.Player(elements[i], copyProperties(config || {}, MTVNPlayer.defaultConfig), events);
+            }
+        };
         Player = function(elementOrId, config, events) {
             this.state = this.currentMetadata = this.playlistMetadata = null;
             /**
@@ -757,9 +942,6 @@ if (!MTVNPlayer.Player) {
              *
              */
             this.config = initConfig(config || {});
-            /**
-             * cross-browser check for element
-             */
             var create = null,
                 el = null,
                 isElement = (function(o) {
@@ -945,142 +1127,6 @@ if (!MTVNPlayer.Player) {
         };
         return Player;
     }(window));
-    /**
-     * Create players from elements in the page.
-     * @param {String} selector default is "div.MTVNPlayer"
-     */
-    MTVNPlayer.createPlayers = function(selector,config,events) {
-        if (!selector) {
-            selector = "div.MTVNPlayer";
-        }
-        if (!MTVNPlayer.selector) {
-            /**
-             * author:  Fabio Miranda Costa
-             * github:  fabiomcosta
-             * twitter: @fabiomiranda
-             * license: MIT-style license
-             */
-            (function(global, document) {
-                var elements, parsed, parsedClasses, parsedPseudos, pseudos = {},
-                    context, currentDocument, reTrim = /^\s+|\s+$/g;
-                var supports_querySelectorAll = !! document.querySelectorAll;
-                var $u = function(selector, _context, append) {
-                        elements = append || [];
-                        context = _context || $u.context;
-                        if (supports_querySelectorAll) {
-                            try {
-                                arrayFrom(context.querySelectorAll(selector));
-                                return elements;
-                            } catch (e) {}
-                        }
-                        currentDocument = context.ownerDocument || context;
-                        parse(selector.replace(reTrim, ''));
-                        find();
-                        return elements;
-                    };
-                var matchSelector = function(node) {
-                        if (parsed.tag) {
-                            var nodeName = node.nodeName.toUpperCase();
-                            if (parsed.tag == '*') {
-                                if (nodeName < '@') return false; // Fix for comment nodes and closed nodes
-                            } else {
-                                if (nodeName != parsed.tag) return false;
-                            }
-                        }
-                        if (parsed.id && node.getAttribute('id') != parsed.id) {
-                            return false;
-                        }
-                        if ((parsedClasses = parsed.classes)) {
-                            var className = (' ' + node.className + ' ');
-                            for (var i = parsedClasses.length; i--;) {
-                                if (className.indexOf(' ' + parsedClasses[i] + ' ') < 0) return false;
-                            }
-                        }
-                        if ((parsedPseudos = parsed.pseudos)) {
-                            for (var i = parsedPseudos.length; i--;) {
-                                var pseudoClass = pseudos[parsedPseudos[i]];
-                                if (!(pseudoClass && pseudoClass.call($u, node))) return false;
-                            }
-                        }
-                        return true;
-                    };
-                var find = function() {
-                        var parsedId = parsed.id,
-                            merge = ((parsedId && parsed.tag || parsed.classes || parsed.pseudos) || (!parsedId && (parsed.classes || parsed.pseudos))) ? arrayFilterAndMerge : arrayMerge;
-                        if (parsedId) {
-                            var el = currentDocument.getElementById(parsedId);
-                            if (el && (currentDocument === context || contains(el))) {
-                                merge([el]);
-                            }
-                        } else {
-                            merge(context.getElementsByTagName(parsed.tag || '*'));
-                        }
-                    };
-                var parse = function(selector) {
-                        parsed = {};
-                        while ((selector = selector.replace(/([#.:])?([^#.:]*)/, parser))) {};
-                    };
-                var parser = function(all, simbol, name) {
-                        if (!simbol) {
-                            parsed.tag = name.toUpperCase();
-                        } else if (simbol == '#') {
-                            parsed.id = name;
-                        } else if (simbol == '.') {
-                            if (parsed.classes) {
-                                parsed.classes.push(name);
-                            } else {
-                                parsed.classes = [name];
-                            }
-                        } else if (simbol == ':') {
-                            if (parsed.pseudos) {
-                                parsed.pseudos.push(name);
-                            } else {
-                                parsed.pseudos = [name];
-                            }
-                        }
-                        return '';
-                    };
-                var slice = Array.prototype.slice;
-                var arrayFrom = function(collection) {
-                        elements = slice.call(collection, 0);
-                    };
-                var arrayMerge = function(collection) {
-                        for (var i = 0, node; node = collection[i++];) {
-                            elements.push(node);
-                        }
-                    };
-                try {
-                    slice.call(document.documentElement.childNodes, 0);
-                } catch (e) {
-                    arrayFrom = arrayMerge;
-                }
-                var arrayFilterAndMerge = function(found) {
-                        for (var i = 0, node; node = found[i++];) {
-                            if (matchSelector(node)) elements.push(node);
-                        }
-                    };
-                var contains = function(node) {
-                        do {
-                            if (node === context) return true;
-                        } while ((node = node.parentNode));
-                        return false;
-                    };
-                $u.pseudos = pseudos;
-                $u.context = document;
-                global.selector = $u;
-            })(MTVNPlayer, document);
-        }
-        var elements = this.selector(selector),
-            configJSON = config ? null : function(found) {
-                if (found && found.length === 1) {
-                    return found[0].innerText;
-                }
-                return;
-            }(this.selector("#MTVNPlayerConfig"));
-        for (var i = 0, len = elements.length; i < len; i++) {
-            new MTVNPlayer.Player(elements[i], config || configJSON, events);
-        }
-    };
     if (typeof MTVNPlayer.onAPIReady === "function") {
         MTVNPlayer.onAPIReady();
     }
