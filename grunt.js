42b790509b4301e68f6d2fa70871c1a0b24c88c3
giannif <giannif@gmail.com>
2012-06-28 12:51:46 -0400

    write the version to MTVNPlayer.version.

--- grunt.js
+++ grunt.js
@@ -1,5 +1,6 @@
 module.exports = function(grunt) {
-    var sourceFiles = ['src/util/module.js', 'src/core.js', 'src/util/config.js', 'src/util/selector.js','src/third-party/swfobject.js','src/player/flash-player.js', 'src/player/html-player.js','src/api.js'],
+    var sourceFiles = ['src/util/module.js', 'src/core.js', 'src/util/config.js', 'src/util/selector.js', 
+    'src/third-party/swfobject.js', 'src/player/flash-player.js', 'src/player/html-player.js', 'src/api.js', 'build/version.js'],
         targetPath = 'build/<%= grunt.config("dirname") %>',
         fileName = '<%= pkg.version %><%= grunt.config("buildNumber") %>.js',
         detailedPath = targetPath + 'detailed/' + fileName,
@@ -8,7 +9,7 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: '<json:package.json>',
         lint: {
-            all: ['grunt.js', 'src/*.js','src/player/*.js','src/util/*.js', 'test/buster/**/*.js']
+            all: ['grunt.js', 'src/*.js', 'src/player/*.js', 'src/util/*.js', 'test/buster/**/*.js']
         },
         min: {
             dist: {
@@ -31,7 +32,7 @@ module.exports = function(grunt) {
                 dest: detailedPath
             },
             auto: {
-                src: sourceFiles.concat(['src/third-party/domready.js','src/auto-create-players.js']),
+                src: sourceFiles.concat(['src/third-party/domready.js', 'src/auto-create-players.js']),
                 dest: autoPath
             }
         },
@@ -39,6 +40,11 @@ module.exports = function(grunt) {
             files: ['grunt.js', 'src/**/*.js', 'test/buster/**/*.js'],
             tasks: 'default'
         }
+    });
+    grunt.registerTask('version', 'write some javascript that contains the version.', function(dir) {
+        var version = grunt.config("pkg").version;
+        grunt.log.writeln("building version:"+version);
+        grunt.file.write("build/version.js", "MTVNPlayer.version=\"" + version + "\";");
     });
     grunt.registerTask('buildNumber', 'append a build number to the build', function(buildNumber) {
         grunt.config("buildNumber", "-" + buildNumber);
@@ -50,6 +56,6 @@ module.exports = function(grunt) {
         }
         grunt.config("dirname", dir);
     });
-    grunt.registerTask('default', 'lint concat');
-    grunt.registerTask('release', 'lint concat min');
+    grunt.registerTask('default', 'version lint concat');
+    grunt.registerTask('release', 'version lint concat min');
 };
