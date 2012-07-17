ad0b052c203edd38f1c2703620d15c5ce8970f39
giannif <giannif@gmail.com>
2012-07-17 13:13:16 -0400

    updating build output. - build     - 2.3.0         - api.js (unminified)         - api.min.js (minified)         - syndicated.js         - auto.js

--- grunt.js
+++ grunt.js
@@ -1,13 +1,15 @@
 module.exports = function(grunt) {
-    var sourceFiles = ['src/util/module.js', 'src/core.js', 'src/util/config.js', 'src/util/selector.js', 
-    'src/third-party/swfobject.js', 'src/player/flash-player.js', 'src/player/html-player.js', 'src/api.js', 'build/version.js'],
-        targetPath = 'build/<%= grunt.config("dirname") %>',
-        fileName = '<%= pkg.version %><%= grunt.config("buildNumber") %>.js',
-        detailedPath = targetPath + 'detailed/' + fileName,
-        autoPath = targetPath + 'auto/' + fileName,
-        minPath = targetPath + fileName;
+    var sourceFiles = ['src/util/start.js', 'src/core.js', 'src/util/config.js', 'src/util/selector.js', 'src/third-party/swfobject.js', 'src/player/flash-player.js', 'src/player/html-player.js', 'src/api.js', 'src/third-party/yepnope.js', 'src/util/reporting.js', 'src/util/load-module.js', 'src/util/finish.js', 'build/version.js'],
+        targetPath = 'build/<%= grunt.config("dirname") %><%= pkg.version %><%= grunt.config("buildNumber") %>/',
+        detailedPath = targetPath + "api.js",
+        autoPath = targetPath + 'auto.min.js',
+        minPath = targetPath + 'api.min.js';
+    grunt.loadNpmTasks('grunt-contrib');
     grunt.initConfig({
         pkg: '<json:package.json>',
+        clean: {
+            folder: ["build/*"]
+        },
         lint: {
             all: ['grunt.js', 'src/*.js', 'src/player/*.js', 'src/util/*.js', 'test/buster/**/*.js']
         },
@@ -42,12 +44,17 @@ module.exports = function(grunt) {
         }
     });
     grunt.registerTask('version', 'write some javascript that contains the version.', function(dir) {
-        var version = grunt.config("pkg").version;
-        grunt.log.writeln("building version:"+version);
-        grunt.file.write("build/version.js", "MTVNPlayer.version=\"" + version + "\";");
+        var version = grunt.config("pkg").version,
+            date = grunt.template.today("mm/dd/yyyy hh:mm:ss");
+        grunt.log.writeln("building version:" + version);
+        grunt.file.write("build/version.js", "MTVNPlayer.version=\"" + version + "\";MTVNPlayer.build=\"" + date + "\";");
     });
     grunt.registerTask('buildNumber', 'append a build number to the build', function(buildNumber) {
         grunt.config("buildNumber", "-" + buildNumber);
+    });
+    grunt.registerTask('finish', 'clean up', function() {
+        grunt.helper("clean", "build/version.js");
+        grunt.log.writeln("Compiled: " + grunt.template.today("mm-dd hh:mm:ss"));
     });
     grunt.registerTask('dirname', 'set a subdirectory name, result will be build/subdirectory(s)', function(dir) {
         if (dir.lastIndexOf("/") !== dir.length - 1) {
@@ -56,6 +63,6 @@ module.exports = function(grunt) {
         }
         grunt.config("dirname", dir);
     });
-    grunt.registerTask('default', 'version lint concat');
-    grunt.registerTask('release', 'version lint concat min');
+    grunt.registerTask('default', 'clean version lint concat finish');
+    grunt.registerTask('release', 'clean version lint concat min finish');
 };
